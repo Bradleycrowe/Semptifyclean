de7e535ccf7c8f4d17e9e53fe90c877679fb7ee7
bradleycrowe <bradcrowe45@gmail.com>
2025-09-12 16:46:34 -0500

    Initial commit for Semptify clean repo

---
+++ src/app.js
@@ -0,0 +1,4 @@
+// Semptify backend placeholder
+console.log("Semptify backend is ready to connect.");
+
+// Future: Firebase integration, complaint storage, user auth
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// Firebase config placeholder
+// We'll fill this in once you create your Firebase project
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Future: initialize Firebase and export services
